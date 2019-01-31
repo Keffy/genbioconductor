93ecc2c36b4316467c08647830c2b86c70536b8e
Keffy <kehrli@gmail.com>
2019-01-31 17:32:59 -0500

    Updating installation instructions for Bioconductor 3.8.

---
+++ R/Install_Bioconductor.R
@@ -0,0 +1,10 @@
+## ----BiocManager, eval=FALSE---------------------------------
+install.packages("BiocManager")
+
+## ----BiocManager1, eval=FALSE--------------------------------
+
+BiocManager::install()
+
+## ----BiocManager2, eval=FALSE--------------------------------
+
+BiocManager::install("limma")
